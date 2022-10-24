efcb460ec247e675852d6ab22793db5404585194
Dolna Sornali <93023509+dolna1610@users.noreply.github.com>
2022-10-24 17:47:44 +0600

    Update CountingCharAndNumber.java

--- src/main/java/string_manipulation/CountingCharAndNumber.java
+++ src/main/java/string_manipulation/CountingCharAndNumber.java
@@ -12,32 +12,10 @@ public static void main(String args[])
     // Given string str
     String str = "Chattogram";
 
-
+        String firstChar= String.valueof(str.toChararray()[0]).toUpperCase ();
+        int length= str.length();
+    String lastChar= String.valueof(str.toChararray()[length-1]).toUpperCase ();
+    String shortCode= firstChar+(length-2)+ lastChar ;
+    System.out.println(shortCode);
 }
-        public static void
-        firstAndLastCharacter(String str)
-        {
-
-            // Finding string length
-            int n = str.length();
-
-            // First character of a string
-            char first = str.charAt(0);
-
-            // Last character of a string
-            char last = str.charAt(n - 1);
-            int count = 0;
-            System.out.println("String: "+str);
-            for (int i = 0; i < str.length(); i++) {
-                if (Character.isLetter(str.charAt(i)))
-                    count++;
-            }
-            System.out.println("Letters: "+count);
-            // Printing first and last
-            // character of a string
-            System.out.println("First: " + first);
-            System.out.println("Last: " + last);
-        }
-
-
-    }
+}
