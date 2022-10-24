d8dcbe63de07f359bb64f393fa722a12217bb5f5
Dolna Sornali <93023509+dolna1610@users.noreply.github.com>
2022-10-24 17:22:53 +0600

    Update ExtractTransactionID.java

--- src/main/java/string_manipulation/ExtractTransactionID.java
+++ src/main/java/string_manipulation/ExtractTransactionID.java
@@ -10,40 +10,22 @@
 package string_manipulation;
 import java.io.*;
 import java.util.*;
-// Importing java URL class
 import java.net.URL;
+
 public class ExtractTransactionID {
-
         public static void main(String[] args)
-                throws FileNotFoundException
         {
+                String html_body="<html>\n" +
+                "<title>Test</title>\n" +
+                "<body>\n" +
+                "Your trnx is successful. Trnx Id is: TXN123456\n" +
+                "</body>\n" +
+                "</html>";
+        int pos= html_body.indexOf("TXN");
+        String txnid = html_body.substring(pos, pos+9);
+        System.out.println(txnid);
+}
+}
+        
 
-            // Try block to check exceptions
-            try {
-                String val;
-
-                URL URL = new URL(
-                        "file:///C:/Users/dolna/IdeaProjects/String-Manipulation");
-
-                // Reading the HTML content from the .HTML File
-                BufferedReader br = new BufferedReader(
-                        new InputStreamReader(URL.openStream()));
-
-            /* Catching the string and  if found any null
-             character break the String */
-                while ((val = br.readLine()) != null) {
-                    System.out.println(val);
-                }
-
-                // Closing the file
-                br.close();
-            }
-
-            // Catch block to handle exceptions
-            catch (Exception ex) {
-
-                // No file found
-                System.out.println(ex.getMessage());
-            }
-        }
-    }
+            
