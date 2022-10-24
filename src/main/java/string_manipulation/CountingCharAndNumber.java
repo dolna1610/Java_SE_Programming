61b7d64f51184101aa96dbf30933038a3da4a026
Dolna Sornali <93023509+dolna1610@users.noreply.github.com>
2022-10-24 17:49:09 +0600

    Update CountingCharAndNumber.java

--- src/main/java/string_manipulation/CountingCharAndNumber.java
+++ src/main/java/string_manipulation/CountingCharAndNumber.java
@@ -10,12 +10,11 @@ public class CountingCharAndNumber {
 public static void main(String args[])
 {
     // Given string str
-    String str = "Chattogram";
-
-        String firstChar= String.valueof(str.toChararray()[0]).toUpperCase ();
-        int length= str.length();
-    String lastChar= String.valueof(str.toChararray()[length-1]).toUpperCase ();
-    String shortCode= firstChar+(length-2)+ lastChar ;
-    System.out.println(shortCode);
-}
+     String str = "Chattogram";
+     String firstChar= String.valueof(str.toChararray()[0]).toUpperCase ();
+     int length= str.length();
+     String lastChar= String.valueof(str.toChararray()[length-1]).toUpperCase ();
+     String shortCode= firstChar+(length-2)+ lastChar ;
+     System.out.println(shortCode);
+ }
 }
