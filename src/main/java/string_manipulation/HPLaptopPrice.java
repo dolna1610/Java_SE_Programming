3b79a2aa78c3324cfedd299a9bc03c69c264f287
Dolna Sornali <93023509+dolna1610@users.noreply.github.com>
2022-10-24 17:37:49 +0600

    Update HPLaptopPrice.java

--- src/main/java/string_manipulation/HPLaptopPrice.java
+++ src/main/java/string_manipulation/HPLaptopPrice.java
@@ -6,20 +6,14 @@ import java.util.Scanner;
 
 public class HPLaptopPrice {
         public static void main(String args[]) {
-            final double d = 0.1;
-            Scanner input = new Scanner(System.in);
-
-            int cashPayment;
-            double purchasePrice;
-            double discounted_price;
-            System.out.print("Enter value of cash payment price: ");
-
-            cashPayment = input.nextInt();
-            discounted_price = cashPayment * d; // Here discount calculation takes place
-            purchasePrice = cashPayment - discounted_price;
-            // Displays discounted price
-            System.out.println("Value of discounted price: " + discounted_price);
-            System.out.println("Value of purchase price: " + purchasePrice);
+            String str="Core i 7 HP laptop price is 76000 tk and cash payment discount 10%. What will be the purchase price?";
+                str=str.replaceAll("[^\\d", " ");
+                str=str.trim();
+                str=str.replaceAll("+"," ");
+                String[] arr= (str.split(" "));
+                int laptopPrice= Integer.parseInt(arr[1]);
+                int discount= Integer.parseInt(arr[2]);
+                int totalPrice= laptopPrice- (laptopPrice*discount)/100;
+                System.out.println("Total price "+totalPrice);
         }
-
-    }
+}
